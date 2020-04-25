2c8e0c9e90aaa99e9f4638ef84dd1c7c84d9682d
basu021 <57030150+basu021@users.noreply.github.com>
2020-04-25 16:39:58 +0530

    Add files via upload

---
+++ modules/update.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+#Tool by- Basudev Rout
+#Instagram- @basudevrout2001
+
+
+
+
+echo ''
+echo ''
+echo ''
+
+figlet -f small Please wait...|lolcat
+
+sleep 1s
+
+figlet -f small Wait |lolcat
+
+
+clear
+sleep 0.6s
+echo ''
+
+echo ''
+echo ''
+echo ''
+
+echo please wait !!!
+cd
+
+rm -rf colorcode
+git clone https://github.com/basu021/colorcode
+
+sleep 0.3s
+echo ''
+echo ''
+
+echo "█...1% completed"
+sleep 0.2s
+echo ''
+echo "█████▓...7% completed"
+sleep 0.3s
+echo " "
+echo "██████████...18% completed"
+
+sleep 0.3s
+echo " "
+echo "████████████████...46% completed"
+sleep 0.5s
+echo " "
+echo "████████████████████████...78% completed"
+sleep 0.3s
+echo " "
+echo "████████████████████████████...83% completed"
+sleep 0.4s
+echo " "
+echo "█████████████████████████████████...93% completed"
+sleep 0.9s
+echo " "
+echo "██████████████████████████████████████...100% completed"
+sleep 0.7s
+echo " "
+echo -e "\e[38;5;120m Done...!\e[0m"
+
+sleep 3s
+echo " "

c877d8fe37d6290149468889b27d0a85c7e29d33
basu021 <57030150+basu021@users.noreply.github.com>
2020-04-26 16:46:56 +0530

    Add files via upload

---
+++ modules/profile.sh
@@ -0,0 +1,9 @@
+echo ' '
+echo -e "     . ……………………………………………………………………………………………."
+echo -e "     : Author:- Basudev Rout     v-0.7    :"
+echo -e "     : Github:- basu021                   :"
+echo -e "     : Insta:- basudevrout2001            :"
+echo -e "     : ……………………………………………………………………………………………:"
+echo ' '
+
+  

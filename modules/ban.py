a97847cb19778ce76683997babfaf1a5d7cf7fe8
basu021 <57030150+basu021@users.noreply.github.com>
2020-04-29 11:36:34 +0530

    Add files via upload

---
+++ modules/ban.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import time
+#colors
+cyan='\033[1;36m'
+reset='\033[0m'
+orn='\033[38;5;202m'
+grn='\033[32m'
+yel='\033[1;49;93m'
+time.sleep(0.01)
+print (cyan  +'             ')
+time.sleep(0.1)
+print (cyan  +' ')
+time.sleep(0.1)
+print (cyan  +'╔═╗┌─┐┌┐┌┌┬┐┌─┐┌─┐┌┬┐')
+time.sleep(0.1)
+print (cyan  +'║  │ ││││ │ ├─┤│   │')
+time.sleep(0.1)
+print (cyan  +'╚═╝└─┘┘└┘ ┴ ┴ ┴└─┘ ┴ ')
+time.sleep(0.1)
+print (cyan +'---------------------')
+time.sleep(0.1)
+print ('   ')
+time.sleep(0.1)
+print (orn +' Feel free to contact me with questions:')
+time.sleep(0.1)
+print ('   ')
+time.sleep(0.1)
+print ('   ')
+time.sleep(0.1)
+print (yel + '1' '⟩' + grn + ' Call')
+time.sleep(0.1)
+print ('   ')
+time.sleep(0.1)
+print (yel + '2' '⟩' + grn + ' Send feedback/message')
+time.sleep(0.1)
+print ('   ')
+time.sleep(0.1)
+print (yel + '3' '⟩' + grn + ' Send message on whatsapp')
+time.sleep(0.1)
+print ('   ')
+time.sleep(0.1)
+print (yel + '4' '⟩' + grn + ' Send message on Instagram')
+time.sleep(0.1)
+print ('   ')
+time.sleep(0.1)
+print (yel + '5' '⟩' + grn + ' Run colorcode again')
+time.sleep(0.1)
+print ('   ')
+time.sleep(0.1)
+print ('   ')
+time.sleep(0.1)
+print ('   ')
---
+++ modules/cb.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# Instagram- @basudevrout2001
+
+
+
+
+
+echo -n "Choose an option to continue: "
+
+read OPT
+
+case $OPT in
+
+  1)
+    termux-telephony-call +917735121328
+    sleep 5s
+    echo "  "
+    OK=`pwd`
+    bash $OK/modules/cb.sh
+    ;;
+  2)
+    echo -n "    "
+    echo -n "Enter your message: "
+    read MSG
+    termux-sms-send -n 8249198256 "$MSG"
+    sleep 4s
+    OK=`pwd`
+    bash $OK/modules/cb.sh
+    ;;
+
+  3)
+    
+    termux-open-url https://wa.me/+918249198256/?text=Type_your_message
+    sleep 4s
+    echo -n "    "
+    OK=`pwd`
+    bash $OK/modules/cb.sh
+    ;;
+
+  5)
+    echo -n "    "
+    echo -n "          Please wait!!!"
+    cd
+    cd colorcode
+    bash run.sh
+    ;;
+  *)
+    echo -n "    "
+    echo -n "Invalid input !\n"
+    sleep 6s
+    OK=`pwd`
+    echo -n "    "
+    bash $OK/modules/cb.sh 
+    ;;
+esac
